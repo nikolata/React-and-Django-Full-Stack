ebd815b191d61330e255f0abe721483a7359b116
nikolata <npmetodiev@gmail.com>
2020-12-02 01:37:28 +0200

    dynamic templates and admin customisation

--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views import View
+from .models import Book
 # Create your views here.
+
+
+def first(request):
+    books = Book.objects.all()
+    return render(request, "first_temp.html", {'books': books})
